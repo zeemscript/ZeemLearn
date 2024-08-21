8c6121ce4f408009740932c6bdbd9370d1fcdeae
Sakariyah Abdulhazeem <sakariyahabdulhazeem@gmail.com>
2024-08-21 11:55:49 +0100

    first

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,11 @@
-"use client"
-import React from 'react';
-import { toast } from "sonner";
-import Hero from './(landingpage)/Hero';
-import About from './(landingpage)/About';
-import Courses from './(landingpage)/Courses';
-import ContactUs from './(landingpage)/Contactus';
-import Suscribe from './(landingpage)/Suscribe';
-import Info from './(landingpage)/Info';
+"use client";
+import React from "react";
+import Hero from "./(landingpage)/Hero";
+import About from "./(landingpage)/About";
+import Courses from "./(landingpage)/Courses";
+import ContactUs from "./(landingpage)/ContactUs"; 
+import Suscribe from "./(landingpage)/Suscribe";
+import Info from "./(landingpage)/Info";
 const Home = () => {
   return (
     <>
@@ -14,10 +13,10 @@ const Home = () => {
       <About />
       <Courses />
       <ContactUs />
-    <Info/>
+      <Info />
       <Suscribe />
     </>
   );
-}
+};
 
 export default Home;
