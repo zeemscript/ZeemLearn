f59df32fffaed38fc1f28a5f6d8497a81c1b3cd3
Sakariyah Abdulhazeem <sakariyahabdulhazeem@gmail.com>
2024-09-05 06:24:32 +0100

    "Refactor AuthContainer component to toggle between login and sign up pages"

--- app/signin/page.jsx
+++ app/signin/page.jsx
@@ -1,19 +1,51 @@
 "use client";
 
-import React from "react";
+import { useState } from "react";
+import Image from "next/image";
 import LoginPage from "@/components/atoms/LoginPage";
 import SignUpPage from "@/components/atoms/SignUpPage";
 
 const AuthContainer = () => {
+  const [isLogin, setIsLogin] = useState(true);
+
+  const handleToggle = () => {
+    setIsLogin(!isLogin);
+  };
+
   return (
-    <div className="flex flex-col md:flex-row justify-center items-center gap-6 p-8 bg-hero bg-cover bg-center min-h-screen">
-      {/* Login Section */}
-      <div className="flex-1 max-w-sm p-6 bg-white shadow-lg rounded-lg border border-gray-200">
-        <LoginPage />
+    <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
+      <div className="flex items-center justify-center py-12">
+        <div className="mx-auto grid w-[350px] gap-6">
+          <div className="grid gap-4">
+            {isLogin ? <LoginPage /> : <SignUpPage />}
+          </div>
+          <div className="mt-4 text-center text-sm">
+            {isLogin ? (
+              <>
+                Don&apos;t have an account?{" "}
+                <button onClick={handleToggle} className="underline">
+                  Sign up
+                </button>
+              </>
+            ) : (
+              <>
+                Already have an account?{" "}
+                <button onClick={handleToggle} className="underline">
+                  Login
+                </button>
+              </>
+            )}
+          </div>
+        </div>
       </div>
-      {/* Sign Up Section */}
-      <div className="flex-1 max-w-sm p-6 bg-white shadow-lg rounded-lg border border-gray-200">
-        <SignUpPage />
+      <div className="hidden lg:block">
+        <Image
+          src="/images/bg-image.jpg"
+          alt="Background"
+          width="1920"
+          height="1080"
+          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
       </div>
     </div>
   );
